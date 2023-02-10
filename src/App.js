ab0c216b8826e500f11bc8f4e5cbcc14771237ef
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2023-02-10 22:08:50 +0100

    Added the hook for the data and displaying one name

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import {useEffect, useState} from "react";
+import {Container} from "semantic-ui-react";
 
 function App() {
   const[data, setData] = useState([]);
@@ -8,37 +9,30 @@ function App() {
   const fetchMarvelHeroes = async() => {
     const url =
         'https://gateway.marvel.com:443/v1/public/characters?ts=1675961395&apikey=824b4f2bb67a85e638a19888121bd05a&hash=d32f224fb3ad8c01de9db005300f1ef6';
-    const response = await fetch(url);
-    const responseJson = await response.json();
+    const results = await fetch(url)
+        .then(res => res.json())
+        .then(res => res.data)
+        .then(res => res.results);
 
-    if (responseJson){
-      setData(responseJson);
-    }
+    setData(results);
 
-  }
+  };
 
   useEffect(() => {
     fetchMarvelHeroes();
   }, []);
 
+
   console.log(data);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+       <div>
+           Hello!{data[5].name}
+
+       </div>
+      </Container>
     </div>
   );
 }
