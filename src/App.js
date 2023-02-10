6ae8649e7badbd8c3a2bda9eda5364f1113a9336
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2023-02-10 17:11:25 +0100

    Added a fetch calling the Marvel API returning a console log with data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+  const[data, setData] = useState([]);
+
+  const fetchMarvelHeroes = async() => {
+    const url =
+        'https://gateway.marvel.com:443/v1/public/characters?ts=1675961395&apikey=824b4f2bb67a85e638a19888121bd05a&hash=d32f224fb3ad8c01de9db005300f1ef6';
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    if (responseJson){
+      setData(responseJson);
+    }
+
+  }
+
+  useEffect(() => {
+    fetchMarvelHeroes();
+  }, []);
+
+  console.log(data);
+
   return (
     <div className="App">
       <header className="App-header">
