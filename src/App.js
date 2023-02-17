3146068f34fc5a711957d7351bd6ac6a8c60dc4c
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2023-02-17 15:27:49 +0100

    Trying to solve some fetch issues, worked briefly but then not

--- src/App.js
+++ src/App.js
@@ -1,52 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 import {useEffect, useState} from "react";
-import {Container} from "semantic-ui-react";
+import {Container, Menu} from "semantic-ui-react";
 
 function App() {
   const[data, setData] = useState([]);
 
-  const fetchMarvelHeroes = async() => {
+    // Black Widow Id= 1009189
+    // Search by name like this: https://gateway.marvel.com:443/v1/public/characters?name=Black%20Widow&ts=1675961395&apikey=824b4f2bb67a85e638a19888121bd05a&hash=d32f224fb3ad8c01de9db005300f1ef6
+    const fetchMarvelHeroes = async() => {
     const url =
         'https://gateway.marvel.com:443/v1/public/characters?ts=1675961395&apikey=824b4f2bb67a85e638a19888121bd05a&hash=d32f224fb3ad8c01de9db005300f1ef6';
-    const results = await fetch(url)
-        .then(res => res.json())
-        .then(res => res.data)
-        .then(res => res.results);
+    const results = await fetch(url);
+    const data = await results.json();
 
-    setData(results);
 
+    return data.data.results;
   };
 
   useEffect(() => {
-    fetchMarvelHeroes();
+      const fn = async() => {
+          const data = await fetchMarvelHeroes();
+          setData(data[4]);
+      }
+    fn();
   }, []);
-
-
-  console.log(data);
 
   return (
     <div className="App">
       <Container>
        <div>
            <div className="ui card">
-               <div className="image">
-                   <img src={data[4].thumbnail.path + "." +data[4].thumbnail.extension}/>
-               </div>
+               {data&&<div className="image">
+                   <img src={data.thumbnail.path + "." + data.thumbnail.extension}
+                        alt={"Thumbnail photo of " + data.name}/>
+               </div>}
                <div className="content">
-                   <a className="header">{data[4].name}</a>
-                   <div className="meta">
-                       <span className="date">Joined in 2013</span>
-                   </div>
-                   <div className="description">
-                       {data[4].description}
-                   </div>
+                   {data&&<a className="header">{data.name}</a>}
+                   {data&&<div className="description">
+                       {data.description}
+                   </div>}
                </div>
                <div className="extra content">
-                   {/*<a>
-                       <i className="user icon"></i>
-                       22 Friends
-                   </a>*/}
                </div>
            </div>
        </div>
